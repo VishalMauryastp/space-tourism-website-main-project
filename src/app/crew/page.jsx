e736f5a3d3d1ed1dabf88927b52a204a0b287a08
Vishal Maurya <vishalmaurya12nov@gmil.com>
2023-09-30 10:43:49 +0530

    crew added

--- src/app/crew/page.jsx
+++ src/app/crew/page.jsx
@@ -1,14 +1,16 @@
-import ComingSoon from '@/components/ComingSoon'
-import React from 'react'
+// import ComingSoon from '@/components/ComingSoon'
+import Carousel from "@/components/Crew/Carousel";
+import React from "react";
 
 const page = () => {
   return (
-    <section className=" h-screen bg-cover bg-no-repeat bg-[url('/starter-code/assets/crew/background-crew-mobile.jpg')] md:bg-[url('/starter-code/assets/crew/background-crew-tablet.jpg')] lg:bg-[url('/starter-code/assets/crew/background-crew-desktop.jpg')]">
-       <div className="pt-[74.4px]">
-        <ComingSoon />
+    <section className="min-h-screen bg-cover bg-no-repeat bg-[url('/starter-code/assets/crew/background-crew-mobile.jpg')] md:bg-[url('/starter-code/assets/crew/background-crew-tablet.jpg')] lg:bg-[url('/starter-code/assets/crew/background-crew-desktop.jpg')]">
+      <div className="pt-[74.4px] ">
+        <Carousel />
+        {/* <ComingSoon /> */}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default page
+export default page;
--- src/app/destination/page.jsx
+++ src/app/destination/page.jsx
@@ -1,4 +1,4 @@
-import ComingSoon from "@/components/ComingSoon";
+// import ComingSoon from "@/components/ComingSoon";
 import Destination from "@/components/Destination/Destination";
 import React from "react";
 
